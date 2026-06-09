344411e8c73a79df523b5efc71beb96703f50824
jj <jo.aoki.0113.jj@gmail.com>
2026-06-09 13:40:04 +0900

    fix: CORS設定を環境変数で設定できるように変更

--- be-todo/src/main.ts
+++ be-todo/src/main.ts
@@ -3,8 +3,11 @@ import { AppModule } from "@/app.module"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  // 3001は本番環境、5173は開発環境
-  app.enableCors({ origin: ["http://localhost:3001", "http://localhost:5173"] })
+  const corsOrigin = process.env.CORS_ORIGIN
+    ? process.env.CORS_ORIGIN.split(",")
+    // 3001は本番環境、5173は開発環境
+    : ["http://localhost:3001", "http://localhost:5173"]
+  app.enableCors({ origin: corsOrigin })
   await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
